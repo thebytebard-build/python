f8c03c2b692f0a1073aa0c54cc9af42f42d32f1e
Roshani Singh Rathore <roshanisinghrathore5@gmail.com>
2025-09-10 21:27:19 +0530

    day 9,10,11,12

---
+++ Day11.py
@@ -0,0 +1,130 @@
+#string part 1
+'''
+# check if string has only alphabet
+s=input('enter a string : ')
+if s.isalpha():
+    print("yes")
+else:
+    print("no")
+
+#check if a given character present in string or not
+s=input('enter a string : ')
+ch=input('enter a string : ')
+if ch in s:
+    print("yes")
+else:
+    print('no')
+
+#count vowels in a string
+#method1
+s=input('enter a string : ')
+vowels='aeiouAEIOU'
+i=0
+for x in vowels:
+    if s.count(x)>0:
+        i=i+s.count(x)
+print('number of vowels : ',i)
+#method 2
+s=input('enter a string : ')
+vowels='aeiouAEIOU'
+i=0
+for x in s:
+    if x in vowels:
+        i=+1
+print('number of vowels : ',i)
+
+#count vowels in a string
+#method 1
+s=input('enter a string : ')
+count=s.count(' ')
+print('number of words : ',count+1)
+#method 2
+s=input('enter a string : ')
+l=s.split()
+print('number of words : ',len(l))
+
+#reverse the string
+#method 1
+s=input('enter a string : ')
+rs=''.join(reversed(s))
+print('reversed string : ',rs)
+#method 2 with slice operator and indexing
+
+#revrse the word of a string
+s=input('enter a string : ')
+l=s.split()
+ln=len(l)-1
+l1=[]
+while 0<=ln:
+    l1.append(l[ln])
+    ln-=1
+rs=' '.join(l1)
+print(rs)
+
+#extract digits from string and store in a list 
+s=input('enter a string : ')
+l=[]
+for x in s:
+    if x.isdigit():
+        l.append(x)
+print(l)
+
+#check if a number is pallindrome
+s=input('enter a string : ')
+rs=''.join(reversed(s))
+if rs==s:
+    print("yes")
+else:
+    print('no')
+
+#convert a string into uppercase
+s=input('enter a string : ')
+us=s.upper()
+print('uppercase string : ',us)
+
+# find maximum length of word in a string
+#method1
+s=input('enter a string :')
+l=s.split()
+maxl=0
+maxw=''
+for x in l:
+    if maxl<len(x):
+        maxl=len(x)
+        maxw=x
+    elif maxl==len(x):
+        maxw=maxw+','+x
+print(maxw,maxl)
+#method 2
+s = input('enter a string : ')
+words = s.split()
+max_len = max(len(w) for w in words)       
+max_words = [w for w in words if len(w) == max_len]  
+print("Longest word(s):", ", ".join(max_words))
+print("Length:", max_len)
+
+#mix of list and str
+
+#remove all no int value from list
+l=input('enter a string : ').split()
+s=''.join(l)
+l.clear()
+i=0
+digits='1234567890'
+for x in s:
+    if x not in digits:
+        l.append(x)
+print(l)
+
+'''
+
+
+
+  
+
+
+
+
+
+
+
---
+++ Day12.py
@@ -0,0 +1,124 @@
+# string part 2
+
+'''
+#count alphabets
+s=input('enter a string : ')
+count=0
+for x in s:
+    if x.isalpha():
+        count+=1
+print(count)
+
+#count number of uppercase and lowercase
+##digit will be ignore by uppercase and lowercase function
+s=input('enter a string : ')
+countu=0
+countl=0
+for x in s:
+    if x.isupper():
+        countu+=1
+    elif x.islower():
+        countl+=1
+
+#convert all alphabet into uppercase
+s=input('enter a string : ')
+output=s.upper()
+print(output)
+
+#convert all alphabet into lowercase
+s=input('enter a string : ')
+output=s.lower()
+print(output)
+
+#convert uppercase into lowercase and lowercase into uppercase
+s=input('enter a string : ')
+output=s.swapcase()
+print(output)
+
+#count all spaces
+s=input('enter a string: ')
+count=0
+for x in s:
+    if x==' ':
+        count+=1
+print(count)
+
+
+#count digit alphabet spaces symbol
+s=input('enter a string : ')
+alpha=0
+digit=0
+symbol=0
+spaces=0
+for x in s:
+    if x.isalpha():
+        alpha+=1
+    elif x.isdigit():
+        digit+=1
+    elif x.isspace():
+        spaces+=1
+    else:
+        symbol+=1
+print('alphabets : ',alpha)
+print('digits : ',digit)
+print('symbols : ',symbol)
+print('spaces : ',spaces)
+
+#reverse word of string
+s=input('enter astring : ')
+l=s.split()
+ln=len(l)-1
+output=''
+while ln>=0:
+    output=output+l[ln]+' '
+    ln-=1
+print(output)
+#method 2
+s=input('enter astring : ')
+l=s.split()
+output=' '.join(reversed(l))
+print(output)
+
+#convert all first letter of word into uppercase and rest into lowercase
+s=input('enter astring : ')
+output=s.title()
+print(output)
+
+#reverse the content of each word in a stirng
+s=input('enter a string : ')
+l=s.split()
+output=''
+for x in l:
+    output=output+x[::-1]+' '
+print(output)
+#method 2
+s = input('enter a string : ')
+l = s.split()
+output = ' '.join([x[::-1] for x in l])
+print(output)
+
+#convert camelcase into snakecase
+s=input('enter a camelcase string : ')
+output=''
+i=0
+while i<len(s):
+    if i==len(s)-1:
+        output=output+s[i]
+    elif s[i+1].isupper():
+        output=output+s[i]+'_'
+    else:
+        output=output+s[i]
+    i+=1
+output=output.lower()
+print(output)
+
+#method2
+s=input('enter a string : ')
+output=''
+for x in s:
+    if x.isupper():
+        output+='_'+x.lower()
+    else:
+        output+=x
+print(output)
+'''
