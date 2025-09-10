177f7ddd8c95af29992b7cefc631e38d465df33e
Roshani Singh Rathore <roshanisinghrathore5@gmail.com>
2025-09-10 21:04:26 +0530

    Day 1 of code

---
+++ Day1.py
@@ -0,0 +1,49 @@
+#PRNT BASIC THINGS
+
+# Print 'MySirG'
+print('MySirG')
+
+# Print multiple lines
+print('''MySirG
+Education Services''')
+
+# Print string with double quotes
+print('"MySirG"')
+
+# Print string with escaped single quote
+print('"Teacher\'s Day"')
+
+# Print backslash and n literally
+print("\\n")
+
+# Print Windows file path
+print('C:\\Users\\John\\Desktop\\File.txt')
+
+# Print string with double quotes
+print('"hello babe"')
+
+# Print string with escaped quotes
+print('He said,\"Hello\"')
+
+# Print string with single quotes inside double quotes
+print("""She said,'it's cold'""")
+
+# Print integer
+a=46
+print(a)
+
+# Print string variable
+name='Roshani'
+print(name)
+
+# Print multiple integers on separate lines
+a,b,c=20,50,63
+print(a,"\n",b,'\n',c,sep="")
+
+# Print different data types
+a,b,c,d,e=35,True,'MySirG',5.46,+4j
+print("int : ",a,"\nbool : ",b,"\nstring : ",c,"\nfloat : ",d,"\ncomplex : ",e)
+
+# Print date in DD/MM/YYYY format
+a,b,c=29,11,2022
+print(a,b,c,sep="/")
